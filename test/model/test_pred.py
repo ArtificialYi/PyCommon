178bae15e37583f68901cf142003cfa26f33b886
huangyi <huangyi@youzan.com>
2023-02-26 13:01:18 +0800

    test: model补充单测

--- test/model/test_pred.py
+++ test/model/test_pred.py
@@ -1,3 +1,4 @@
+import torch
 from ...src.model.pred import PyramidOut
 
 
@@ -13,5 +14,10 @@ class TestPyramidOut:
         m_pred = PyramidOut(history_num, output_num, hidden_num).double().eval()
         assert len(list(m_pred.named_parameters())) == (hidden_num + 3) * 10
         assert not m_pred.training
+
+        data_history = torch.randn(history_num).double()
+        data_output0 = m_pred.forward(data_history)
+        data_output1 = m_pred(data_history)
+        assert torch.allclose(data_output0, data_output1)
         pass
     pass
