c359ed12a9d8d3ab400b9f639326b64d81b95b8d
huangyi <huangyi@youzan.com>
2023-05-03 15:58:09 +0800

    feat: 添加sql_tool

--- src/tool/sql_tool.py
+++ src/tool/sql_tool.py
@@ -1,5 +1,38 @@
+import re
+from typing import List
+
+
 class Mysql2Other:
     @staticmethod
     def sqlite(sql: str):
         return sql.replace('%s', '?')
+    pass
+
+
+class SQLTool:
+    __FIELD_RE = re.compile(r'_lst\.')
+
+    @classmethod
+    def rule2name(cls, field_rule: str):
+        """将field_rule转化为field_name
+        1. 将rule中的_lst.转化为_
+        """
+        return cls.__FIELD_RE.sub('_', field_rule)
+
+    @classmethod
+    def rule2value(cls, dbt: object, field_rule: str):
+        attr_lst = cls.__FIELD_RE.split(field_rule)
+        value = getattr(dbt, attr_lst[0] if len(attr_lst) == 1 else f'{attr_lst[0]}_lst')
+        for idx_str in attr_lst[1:]:
+            value = value[int(idx_str)]
+            pass
+        return value
+
+    @staticmethod
+    def to_sql_fields(fields: List[str]):
+        return f'`{"`,`".join(fields)}`'
+
+    @staticmethod
+    def to_sql_format(fields: List[str]):
+        return ','.join([f'%({field})s' for field in fields])
     pass
