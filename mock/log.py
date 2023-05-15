14b1ae3c1c9545c9ed4929add486fb18d1e71faf
huangyi <huangyi@youzan.com>
2023-05-15 15:44:07 +0800

    test: mock_logger

--- mock/log.py
+++ mock/log.py
@@ -1,8 +1,34 @@
-from aiologger import Logger
-
 from ..src.tool.map_tool import MapKey
 
 
 @MapKey()
 async def get_mock_logger():
-    return Logger.with_default_handlers()
+    return MockLogger()
+
+
+class MockLogger:
+    @staticmethod
+    async def debug(*args, **kwds):
+        print(*args, **kwds)
+        pass
+
+    @staticmethod
+    async def info(*args, **kwds):
+        print(*args, **kwds)
+        pass
+
+    @staticmethod
+    async def warning(*args, **kwds):
+        print(*args, **kwds)
+        pass
+
+    @staticmethod
+    async def error(*args, **kwds):
+        print(*args, **kwds)
+        pass
+
+    @staticmethod
+    async def exception(*args, **kwds):
+        print(*args, **kwds)
+        pass
+    pass
