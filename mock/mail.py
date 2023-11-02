4e08d8142b88d19c94bc808955472e124fd1dc46
huangyi <496109870@qq.com>
2023-11-02 20:46:36 +0800

    test: mail单测

--- mock/mail.py
+++ mock/mail.py
@@ -1,16 +1,17 @@
-
-
+from attr import dataclass
 from pytest_mock import MockerFixture
+
 from ..src.dependency import mail
 
 
+@dataclass
 class MockMail:
     @staticmethod
     def mock_init(mocker: MockerFixture):
-        mock = mocker.patch(f'{mail.__name__}.MailManage.__new__')
-        mock.return_value = MockMail()
+        mocker.patch(f'{mail.__name__}.Mail', new=MockMail)
         pass
 
     def send(self, *args, **kwds) -> None:
+        print(f'单测-发送邮件-{args}-{kwds}')
         pass
     pass
